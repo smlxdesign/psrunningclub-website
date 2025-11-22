0dc531af1cacc473561b6e0e00a445efc4b67e54
Samuel <samuel.smlxdesign@gmail.com>
2025-11-22 22:38:17 +0000

    style: add biome config

--- scripts/get-instagram-post.js
+++ scripts/get-instagram-post.js
@@ -7,40 +7,40 @@ process.loadEnvFile();
 const API_URL = "https://graph.instagram.com/v24.0";
 
 const data = await fetch(
-  `${API_URL}/${process.env.ACCOUNT_ID}/media?fields=username,media_url,media_type,thumbnail_url`,
-  {
-    headers: {
-      Authorization: `Bearer ${process.env.AUTHORIZATION_KEY}`,
+    `${API_URL}/${process.env.ACCOUNT_ID}/media?fields=username,media_url,media_type,thumbnail_url`,
+    {
+        headers: {
+            Authorization: `Bearer ${process.env.AUTHORIZATION_KEY}`,
+        },
     },
-  },
 ).then((res) => {
-  return res.json();
+    return res.json();
 });
 
 const latestPost = data.data
-  .filter((post) => {
-    return !!post.media_url;
-  })
-  .toSorted((a, b) => {
-    return new Date(b.timestamp) - new Date(a.timestamp);
-  })[0];
+    .filter((post) => {
+        return !!post.media_url;
+    })
+    .toSorted((a, b) => {
+        return new Date(b.timestamp) - new Date(a.timestamp);
+    })[0];
 
 const originialHtml = await readFile("../index.html", "utf-8");
 
 const containerElementRegex = /(<instagram-post>)(.*)(<\/instagram-post>)/gs;
 
 await writeFile(
-  "../index.html",
-  originialHtml.replace(
-    containerElementRegex,
-    `
+    "../index.html",
+    originialHtml.replace(
+        containerElementRegex,
+        `
         <instagram-post>
             ${
-              latestPost.media_type === "VIDEO"
-                ? `<video controls width="350" poster="${latestPost.thumbnail_url}">
+                latestPost.media_type === "VIDEO"
+                    ? `<video controls width="350" poster="${latestPost.thumbnail_url}">
                 <source src="${latestPost.media_url}">
               </video>`
-                : `<img width="350" src="${latestPost.media_url}" />`
+                    : `<img width="350" src="${latestPost.media_url}" />`
             }
             <div>
                 <p>
@@ -51,5 +51,5 @@ await writeFile(
             </div>
         </instagram-post>
     `.trim(),
-  ),
+    ),
 );
